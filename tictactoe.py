17a6ce9cb1990dcacc49ae5a722bbf854ed574ab
parseltongist <>
2021-12-24 01:59:11 +0100

    Initial commit

--- tictactoe.py
+++ tictactoe.py
@@ -1,179 +1,69 @@
-# Tic-Tac-Toe game for 2 players. X moves 1st then O.
-# The fist who will put own sign 3 time in row, column or horizontally - wins.
-# In all cells are occupied and there is no winner - it's a "Draw", game will be over with no winner.
+class TicTacToe:
 
-# all functions will be on top. basic logic and varialbes on the bottom
-def print_grid():
-    global top, mid, bot
-    horizontal_border =  9 * "-"
-    print(horizontal_border)
-    print("| {} {} {} |".format(top[0],top[1], top[2]))  # printing symbols separately so it's possible to have a blankspace between each of three sybmols
-    print("| {} {} {} |".format(mid[0],mid[1], mid[2]))
-    print("| {} {} {} |".format(bot[0],bot[1], bot[2]))
-    print(horizontal_border)
+    def __init__(self):
+        self.curr_player_sign = "X"  # default value as "X" makes the 1st move
+        self.grid = [['_', '_', '_'] for _ in range(3)]
+        self.top, self.mid, self.bot =\
+            self.grid[0], self.grid[1], self.grid[2]
 
+    def print_grid(self):
+        print(f"{9 * '-'}\n| {' '.join(self.top)} |\n| {' '.join(self.mid)} |\n| {' '.join(self.bot)} |\n{9 * '-'}")  # printing symbols separately so it's possible to have a blankspace between each of three sybmols
 
-def possible_combinations():
-    # accessing global variables to make sure operations within function will change the global value
-    global toprow_win, midrow_win, botrow_win, leftcol_win, midcol_win, rightcol_win, ldialonal, rdialonal
-    # possible combinations list :
-    # cheching row combinations
-    underscore = "_"
-    if top[0] == top[1] == top[2] != underscore:  # checking if all symbols in row are the same and allow to win
-        toprow_win = True
-    else:
-        toprow_win = False
-    if mid[0] == mid[1] == mid[2] != underscore:  # checking if all symbols in row are the same and allow to win
-        midrow_win = True
-    else:
-        midrow_win = False
-    if bot[0] == bot[1] == bot[2] != underscore:  # checking if all symbols in row are the same and allow to win
-        botrow_win = True
-    else:
-        botrow_win = False
-    # checking column combinations
-    if top[0] == mid[0] == bot[0] != underscore:  # checking if all symbols in column are the same and allow to win
-        leftcol_win = True
-    else:
-        leftcol_win = False
-    if top[1] == mid[1] == bot[1] != underscore:  # checking if all symbols in column are the same and allow to win
-        midcol_win = True
-    else:
-        midcol_win = False
-    if top[2] == mid[2] == bot[2] != underscore:  # checking if all symbols in column are the same and allow to win
-        rightcol_win = True
-    else:
-        rightcol_win = False
-    # Checking diagonals:
-    if top[0] == mid[1] == bot[2] != underscore:  # checking if all symbols in diagonal are the same and allow to win
-        ldialonal = True
-    else:
-        ldialonal = False
-    if top[2] == mid[1] == bot[0] != underscore:  # checking if all symbols in diagonal are the same and allow to win
-        rdialonal = True
-    else:
-        rdialonal = False
+    def switch_player(self):
+        self.curr_player_sign = "O" if self.curr_player_sign == "X" else "X"
 
+    def user_move(self):
+        try:
+            row, column = [int(x) - 1 for x in input("Enter the coordinates:").split()]
+            if self.grid[row][column] in "XO":
+                print("This cell is occupied! Choose another one!")
+                self.user_move()
+            else:
+                self.grid[row][column] = self.curr_player_sign
+                self.print_grid()
+        except ValueError:
+            print("You should enter numbers!")
+            return self.user_move()
+        except IndexError:
+            print("Coordinates should be from 1 to 3!")
+            return self.user_move()
 
-def next_player():
-    global current_player
-    if current_player == player_x:
-        current_player = player_o
-    else:
-        current_player = player_x
+    def game_rules_validation(self):
+        """ Tic-Tac-Toe game for 2 players. X moves 1st then O.
+            The fist who will put own sign 3 time in row, column or horizontally - wins.
+            In all cells are occupied and there is no winner - it's a "Draw", game will be over with no winner.
+        """
+        empty_cells = (self.top.count("_") + self.mid.count("_") + self.bot.count("_"))
 
+        if any([self.top[0] == self.top[1] == self.top[2] != "_",  # top row
+                self.top[0] == self.mid[0] == self.bot[0] != "_",  # left column
+                self.top[0] == self.mid[1] == self.bot[2] != "_",  # second diagonal
+                ]):
+            print(f"{self.top[0]} wins")
+            exit()
 
-def user_move(step):
-    global current_field
-    row, column = input("Enter the coordinates:").split()
-    try:
-        column = int(column)
-        row = int(row)
-        column -= 1
-        row -= 1
-        if current_field[row][column] == "X":
-            pass
-        if current_field[row][column] == "O":
-            pass
-    except ValueError:
-        print("You should enter numbers!")
-        user_move(step)
-    except IndexError:
-        print("Coordinates should be from 1 to 3!")
-        user_move(step)
-    else:
-        if current_field[row][column] == "X" or current_field[row][column] == "O":
-            print("This cell is occupied! Choose another one!")
-            user_move(step)
-        else:
-            current_field[row][column] = step
-            print_grid()
+        elif any([self.mid[0] == self.mid[1] == self.mid[2] != "_",  # middle line
+                  self.top[1] == self.mid[1] == self.bot[1] != "_",  # middle column
+                  self.top[2] == self.mid[1] == self.bot[0] != "_",  # main diagonal
+                  ]):
+            print(f"{self.mid[1]} wins")
+            exit()
 
+        elif any([self.bot[0] == self.bot[1] == self.bot[2] != "_",  # bottom line
+                  self.top[2] == self.mid[2] == self.bot[2] != "_"]  # right column
+                 ):
+            print(f"{self.bot[2]} wins")
+            exit()
 
-def game_rules_validation():
-    global current_field, top, mid, bot, toprow_win, midrow_win, botrow_win, leftcol_win, midcol_win, rightcol_win
-    # checking if game is possible
-    empty_cells = (top.count("_") + mid.count("_") + bot.count("_"))
-    #looking for a winner on a row level
-    if toprow_win:
-        if top[0] == "O":
-            print("O wins")
-            exit()
-        else:
-            print("X wins")
-            exit()
-    elif midrow_win:
-        if mid[0] == "O":
-            print("O wins")
-            exit()
-        else:
-            print("X wins")
-            exit()
-    elif botrow_win:
-        if bot[0] == "O":
-            print("O wins")
-            exit()
-        else:
-            print("X wins")
-            exit()
-    #looking for a winner on a column level
-    elif leftcol_win:
-        if top[0] == "O":
-            print("O wins")
-            exit()
-        else:
-            print("X wins")
-            exit()
-    elif midcol_win:
-        if top[1] == "O":
-            print("O wins")
-            exit()
-        else:
-            print("X wins")
-            exit()
-    elif rightcol_win:
-        if top[2] == "O":
-            print("O wins")
-            exit()
-        else:
-            print("X wins")
-            exit()
-    #looking for a winner on a diagonal level
-    elif ldialonal or rdialonal:
-        if mid[1] == "O":
-            print("O wins")
-            exit()
-        else:
-            print("X wins")
-            exit()
-    else:
-        if empty_cells < 1:  # at least 1 move left  # issue is here, DRAW function is not working
+        elif empty_cells < 1:  # at least 1 move left  # issue is here, DRAW function is not working
             print("Draw")
             exit()
-        else:
-            pass
 
-###### The first launch #####################################
-# creating check-variables, describing potential scenarios
-toprow_win, midrow_win, botrow_win, leftcol_win, midcol_win, rightcol_win, ldialonal, rdialonal = None, None, None, None, None, None, None, None
-# creating & asigning basic variables:
-player_x = "X"  # defining 1st player sign
-player_o = "O"  # defining 2nd player sign
-current_player = player_x  # default value as "X" makes the 1st move
-current_field = [['_','_','_'], ['_', '_', '_'], ['_', '_', '_']]  # assigning default starting grid / field
-# creating additional abstraction level to improve code readability
-top = current_field[0]
-mid = current_field[1]
-bot = current_field[2]
-print_grid()  # print 1st clear grid/field
 
-# main logic / gameplay loop
-while True:
-    possible_combinations()  # testing if adding this function here will prevent game continuation once won
-    game_rules_validation()
-    if current_player == player_x:
-        user_move(player_x)
-        next_player()
-    else:
-        user_move(player_o)
-        next_player()
+if __name__ == '__main__':
+    game = TicTacToe()
+    game.print_grid()  # print 1st clear grid/field
+    while True:
+        game.game_rules_validation()
+        game.user_move()
+        game.switch_player()
